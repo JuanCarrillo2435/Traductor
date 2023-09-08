77a67245225c52730485c72ef3051764dc02e0be
JuanCarrillo2435 <142348563+JuanCarrillo2435@users.noreply.github.com>
2023-09-08 16:59:37 -0300

    Add files via upload

---
+++ Traductor2/src/traductor2/Traductor2.java
@@ -0,0 +1,7 @@
+package traductor2;
+import java.util.Scanner;
+public class Traductor2 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    }
+}
